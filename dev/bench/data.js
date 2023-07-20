c327bc388931c266ecbfe3ba80beb13e1492f39f
github-action-benchmark <github@users.noreply.github.com>
2023-07-20 18:21:36 +0000

    add Rust Benchmark (cargo) benchmark result for ae5bda4692d68dab8491893d6eb23a3640712135

---
+++ dev/bench/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1689877296501,
+  "repoUrl": "https://github.com/wnfs-wg/deterministic-bloom",
+  "entries": {
+    "Rust Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "hello@brooklynzelenka.com",
+            "name": "Brooklyn Zelenka",
+            "username": "expede"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ae5bda4692d68dab8491893d6eb23a3640712135",
+          "message": "Port from `rs-wnfs` (#2)\n\nCo-authored-by: Philipp Krüger <philipp.krueger1@gmail.com>",
+          "timestamp": "2023-07-20T20:20:07+02:00",
+          "tree_id": "1a18a97802c868fa0022adbcde02fbe9dfa98b12",
+          "url": "https://github.com/wnfs-wg/deterministic-bloom/commit/ae5bda4692d68dab8491893d6eb23a3640712135"
+        },
+        "date": 1689877295424,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "add",
+            "value": 535,
+            "range": "± 1",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "count_ones",
+            "value": 618,
+            "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
